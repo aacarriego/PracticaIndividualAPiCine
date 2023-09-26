c6de465a835cbd1fab1b5bb33aee105e8f42d095
Ariel Carriego <aa.carriego@gmail.com>
2023-09-26 20:12:22 -0300

    SOLO funciona get generos

--- Application/UseCase/FuncionesService.cs
+++ Application/UseCase/FuncionesService.cs
@@ -23,57 +23,57 @@ namespace Application.UseCase
             _command = command;
         }
 
-        //public Task<Funcion> CreateFuncion(FuncionPostDTO request)
-        //{
-        //    /// uso libreria automapping  
-        //    var funcion = _mapper.Map<Funcion>(request);  
-
-        //    _command.InsertFuncion(funcion);
-        //    return Task.FromResult(funcion);
-
-
-        //}
-
-        [HttpPost]
-        public async Task<ActionResult<FuncionDTO>> PostFuncion(FuncionPostDTO request)
+        public Task<Funcion> CreateFuncion(FuncionPostDTO request)
         {
-            // Verifica si hay funciones existentes en la misma sala que se superponen en tiempo
-            DateTime nuevaFuncionInicio = request.Horario; // Corrección aquí
-            DateTime nuevaFuncionFin = nuevaFuncionInicio.AddMinutes(150); // 2 horas y 30 minutos
+            /// uso libreria automapping  
+            var funcion = _mapper.Map<Funcion>(request);
 
-            bool superposicion = await _context.Funciones
-                .AnyAsync(f =>
-                    f.SalaId == funcionPOST.SalaId &&
-                    f.Fecha.Date == funcionPOST.Fecha.Date && // Mismo día
-                    f.Horario <= nuevaFuncionFin && f.Horario.AddMinutes(150) >= nuevaFuncionInicio); // Superposición de horarios
+            _command.InsertFuncion(funcion);
+            return Task.FromResult(funcion);
 
-            if (superposicion)
-            {
-                return BadRequest("La nueva función se superpone con otra función en la misma sala.");
-            }
 
-            var funcion = new Funcion
-            {
-                SalaId = funcionPOST.SalaId,
-                Fecha = funcionPOST.Fecha,
-                Horario = funcionPOST.Horario,
-                PeliculaId = funcionPOST.PeliculaId
-            };
-
-            _context.Funciones.Add(funcion);
-            await _context.SaveChangesAsync();
-
-            // Devuelve la función creada
-            var funcionDTO = new FuncionDTO
-            {
-                SalaId = funcion.SalaId,
-                Fecha = funcion.Fecha,
-                Horario = funcion.Horario,
-                PeliculaId = funcion.PeliculaId
-            };
-
-            return CreatedAtAction("GetFuncion", new { id = funcion.FuncionId }, funcionDTO);
         }
+
+
+        //public async Task<ActionResult<FuncionDTO>> PostFuncion(FuncionPostDTO request)
+        //{
+        //    // Verifica si hay funciones existentes en la misma sala que se superponen en tiempo
+        //    DateTime nuevaFuncionInicio = request.Horario; // Corrección aquí
+        //    DateTime nuevaFuncionFin = nuevaFuncionInicio.AddMinutes(150); // 2 horas y 30 minutos
+
+        //    bool superposicion = await _context.Funciones
+        //        .AnyAsync(f =>
+        //            f.SalaId == funcionPOST.SalaId &&
+        //            f.Fecha.Date == funcionPOST.Fecha.Date && // Mismo día
+        //            f.Horario <= nuevaFuncionFin && f.Horario.AddMinutes(150) >= nuevaFuncionInicio); // Superposición de horarios
+
+        //    if (superposicion)
+        //    {
+        //        return BadRequest("La nueva función se superpone con otra función en la misma sala.");
+        //    }
+
+        //    var funcion = new Funcion
+        //    {
+        //        SalaId = funcionPOST.SalaId,
+        //        Fecha = funcionPOST.Fecha,
+        //        Horario = funcionPOST.Horario,
+        //        PeliculaId = funcionPOST.PeliculaId
+        //    };
+
+        //    _context.Funciones.Add(funcion);
+        //    await _context.SaveChangesAsync();
+
+        //    // Devuelve la función creada
+        //    var funcionDTO = new FuncionDTO
+        //    {
+        //        SalaId = funcion.SalaId,
+        //        Fecha = funcion.Fecha,
+        //        Horario = funcion.Horario,
+        //        PeliculaId = funcion.PeliculaId
+        //    };
+
+        //    return CreatedAtAction("GetFuncion", new { id = funcion.FuncionId }, funcionDTO);
+        //}
 
         public Task<Funcion> DeleteFuncion(int FuncionId)
         {
